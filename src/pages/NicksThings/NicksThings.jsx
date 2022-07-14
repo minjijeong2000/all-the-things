fdb9bbcfc85ebddc213ec30fd55cb04789d7a4b3
Nicholas Bailey <nickcyg1992@gmail.com>
2022-07-14 12:40:18 -0500

    add Nick's Things

---
+++ src/pages/NicksThings/NicksThings.jsx
@@ -0,0 +1,18 @@
+import ThingCard from '../../components/ThingCard/ThingCard'
+
+import { Link } from 'react-router-dom'
+
+const NicksThings = (props) => {
+  return (
+    <>
+      <h1>Nick's Things </h1>
+      <Link to="/">Home</Link>
+
+      {props.things.map((thing, idx) => 
+        <ThingCard key={idx} thing={thing}/>
+      )}
+    </>
+  )
+}
+ 
+export default NicksThings
